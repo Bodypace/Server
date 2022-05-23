068623b40b1b9c963a690c0abb66e96217c05789
Robert Dorna <rdorna8@gmail.com>
2022-05-23 08:48:27 +0200

    feat-dev: implement user register logic

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -3,16 +3,31 @@ const {
   hashPassword, protect
 } = require('@feathersjs/authentication-local').hooks;
 const { authorize } = require('../../hooks/authorize');
-const { iff } = require('feathers-hooks-common');
+const { iff, debug } = require('feathers-hooks-common');
 
 const isLoggedIn = () => context => context.params.user !== undefined;
+
+const register = () => {
+  return async context => {
+    const { code } = context.data
+
+    if (code === undefined) {
+      console.log('sending confirm code to email: ABC11')
+    }
+    else {
+      console.log("does code match: ", (code === "ABC11"))
+    }
+
+    return context
+  }
+}
 
 module.exports = {
   before: {
     all: [],
     find: [ authenticate('jwt'), iff(isLoggedIn(), authorize()) ],
     get: [ authenticate('jwt'), iff(isLoggedIn(), authorize()) ],
-    create: [ hashPassword('password') ],
+    create: [ hashPassword('password'), debug('debug user'), register() ],
     update: [ hashPassword('password'),  authenticate('jwt'), authorize() ],
     patch: [ hashPassword('password'),  authenticate('jwt'), authorize() ],
     remove: [ authenticate('jwt'), authorize() ]
