e74a8c7835cfcc07fa4c54afc05cb34ce88ee23d
Robert Dorna <rdorna8@gmail.com>
2022-07-13 18:12:21 +0200

    refactor: remove unncessary debug log

--- src/hooks/authorize.js
+++ src/hooks/authorize.js
@@ -1,30 +1,26 @@
 const { setField } = require('feathers-authentication-hooks');
-const logger = require('../logger');
 
 module.exports = {
-
   authorize: () => {
-
     const from = 'params.user.id';
-    return async context => {
+    return async (context) => {
       const { path, method, type } = context;
-      logger.debug(`authorizing request path: ${path}, method: ${method}, type: ${type}`);
 
       if (type !== 'before') {
-        throw Error(`Tried to authorize service in "${type}" hook instead of "before"`);
+        throw Error(
+          `Tried to authorize service in "${type}" hook instead of "before"`
+        );
       }
 
       return setField({
-        from, as:
+        from,
+        as:
           path === 'users'
             ? 'params.query.id'
             : method === 'create'
               ? 'data.UserId'
-              : 'params.query.UserId'
+              : 'params.query.UserId',
       })(context);
-
     };
-
-  }
-
+  },
 };
